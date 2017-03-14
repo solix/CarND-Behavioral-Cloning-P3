00d2c9ff370d5873918eab3b879e67c8d6634a95
solix <soheil.jahanshahi@gmail.com>
2017-03-14 18:12:35 +0100

    reduced epoch to 3

--- model.py
+++ model.py
@@ -29,5 +29,5 @@ model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss = 'mse' , optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True , nb_epoch = 7 )
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True , nb_epoch = 3 )
 model.save('model.h5')
