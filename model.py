bd00b29132468860575142c84e3707b1634d0891
solix <soheil.jahanshahi@gmail.com>
2017-03-14 18:23:30 +0100

    experimental model with more layers

--- model.py
+++ model.py
@@ -22,10 +22,15 @@ y_train = np.array(labels)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D,MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0 - 0.5,input_shape = (160,320,3)))
+model.add(Convolution2D(6,3,3,activation='relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(240))
+model.add(Dense(120))
 model.add(Dense(1))
 
 model.compile(loss = 'mse' , optimizer = 'adam')
