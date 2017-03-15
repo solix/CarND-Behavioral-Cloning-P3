85e719fcde60f1e0c7bd650c133d82441330d62c
solix <soheil.jahanshahi@gmail.com>
2017-03-15 21:14:48 +0100

    resizing again

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ for image , mesure in zip(imgs,labels):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-X_train = np.reshape(X_train,[X_train.shape[0],66,200,3])
+X_train = np.reshape(X_train,[len(X_train),66,200,3])
 
 x_train = X_train.astype('float32')
 
