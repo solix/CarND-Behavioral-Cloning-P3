c12034ef111c0c36fb03a0fb0d947a3e9e82e403
solix <soheil.jahanshahi@gmail.com>
2017-03-15 19:08:32 +0100

    cropped had error

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ def main(_):
 
     model = Sequential()
     model.add(Lambda(lambda x:x/255.0 - 0.5,input_shape = (160,320,3)))
-    model.add(Cropping2D((50,20),(0,0)))
+    model.add(Cropping2D(cropping=((70,25),(0,0))))
     model.add(Convolution2D(6,3,3,activation='relu'))
     model.add(MaxPooling2D())
     model.add(Flatten())
