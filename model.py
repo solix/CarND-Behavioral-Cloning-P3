1351418a83eebcd0e8ece5b44a0fe383add8bdf8
solix <soheil.jahanshahi@gmail.com>
2017-03-15 21:37:20 +0100

    resizing again using Keras core

--- model.py
+++ model.py
@@ -59,7 +59,7 @@ def main(_):
     model = Sequential()
     model.add(Lambda(lambda x:x/255.0 - 0.5,input_shape = (160,320,3)))
     model.add(Cropping2D(cropping=((70,25),(0,0))))  # also supports shape inference using `-1` as dimension
-    model.add(Reshape((-1,66, 200,3)))
+    model.add(Lambda(lambda x: tf.image.resize_images(x, (66, 200))))
     model.add(Convolution2D(3,5,5,activation='relu'))
     model.add(MaxPooling2D())
     model.add(Dropout(0.25))
