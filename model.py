89d5b697b4a497da352a70ed4b0ff19bfaf9779a
solix <soheil.jahanshahi@gmail.com>
2017-03-20 06:20:36 +0000

    changed model to nvidia with left and right images

--- model.py
+++ model.py
@@ -11,26 +11,27 @@ reader = pd.read_csv('./data/driving_log.csv', usecols=['center', 'left', 'right
 imgs = []
 labels = []
 for index, row in reader.iterrows():
-    # print(row['center'], row['steering'])
-    for i in range(3):
-        source =  row[i]
-        token = source.split('/')
-        local_path = './data/IMG/'
-        file_path = token[-1]
-        local_path = local_path+file_path
-        img = cv2.imread(local_path)
-        imgs.append(img)
-    steering = float(row['steering'])
-    labels.append(steering)
-    labels.append(steering + 0.1)
-    labels.append(steering - 0.1)
+    
+    for i in range(3):      
+         source =  row[i]
+         token = source.split('/')
+         local_path = './data/IMG/'
+         file_path = token[-1]
+         local_path = local_path+file_path
+         img = cv2.imread(local_path)
+         imgs.append(img)
+         steering = float(row['steering'])
+         labels.append(steering)
+         labels.append(steering -0.2)
+	 labels.append(steering +0.2)
 
 augmented_images, augmented_measurements = [], []
 
 for image, mesure in zip(imgs, labels): 
     augmented_images.append(image)
+    augmented_measurements.append(mesure)
     augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(mesure * -1.0)
+    augmented_measurements.append(mesure*-1.0)
 
 X_train = np.array(imgs)
 y_train = np.array(labels)
@@ -57,27 +58,25 @@ def main(_):
     print('Build model...')
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Cropping2D(cropping=((50, 20), (0, 0))))  # also supports shape inference using `-1` as dimension
-    print(model.output)  # shape=(?, 66, 200, 3)
+    model.add(Cropping2D(cropping=((70,20), (0, 0))))  # also supports shape inference using `-1` as dimension
     model.add(Convolution2D(3, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Dropout(0.55))
+    model.add(Dropout(0.75))
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Dropout(0.55))
+    model.add(Dropout(0.75))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Dropout(0.55))
+    model.add(Dropout(0.75))
     model.add(Convolution2D(48, 3, 3, activation='relu'))
-    model.add(Dropout(0.55))
+    model.add(Dropout(0.75))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
-    model.add(Dropout(0.75))
     model.add(Dense(10))
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer=Adam(lr=FLAGS.learning_rate))
     print("Model summary:\n", model.summary())
 
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 1)
     model.save('model.h5')
     print("Model is saves as model.h5")
 
