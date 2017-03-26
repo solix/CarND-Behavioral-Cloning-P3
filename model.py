9687d92ad477455730e1735bfb7a6fe8c6e0b54d
solix <soheil.jahanshahi@gmail.com>
2017-03-26 10:37:00 +0000

    added modified model.py with recovery data

--- model.py
+++ model.py
@@ -7,19 +7,34 @@ from keras import backend as K
 from keras.layers.advanced_activations import ELU
 from keras.regularizers import l2, activity_l2
 
-reader = pd.read_csv('./data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
-my_reader = pd.read_csv('./my_data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
-my_reader_offroad = pd.read_csv('./my_data_2/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
-my_reader_offroad_2 = pd.read_csv('./my_data_3/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
-
+#reader = pd.read_csv('./data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+#my_reader = pd.read_csv('./my_data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+#my_reader_2 = pd.read_csv('./my_data_2/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+drive_reader = pd.read_csv('./track1/drive/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+recovery_reader = pd.read_csv('./track1/recovery/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 imgs = []
 labels = []
-for  index, row in reader.iterrows():
+#for  index, row in reader.iterrows():
+    # print(row['center'], row['steering'])
+    #for i in range(3):
+     #   source =  row[i]
+      #  token = source.split('/')
+       # local_path = './data/IMG/'
+        #file_path = token[-1]
+        #local_path = local_path+file_path
+       # img = cv2.imread(local_path)
+       # imgs.append(img)
+   # steering = float(row['steering'])
+   # labels.append(steering)
+   # labels.append(steering + 0.2)
+   # labels.append(steering - 0.2)
+
+for index, row in drive_reader.iterrows():
     # print(row['center'], row['steering'])
     for i in range(3):
         source =  row[i]
         token = source.split('/')
-        local_path = './data/IMG/'
+        local_path = './track1/drive/IMG/'
         file_path = token[-1]
         local_path = local_path+file_path
         img = cv2.imread(local_path)
@@ -29,47 +44,16 @@ for  index, row in reader.iterrows():
     labels.append(steering + 0.2)
     labels.append(steering - 0.2)
 
-for index, row in my_reader.iterrows():
-    # print(row['center'], row['steering'])
-    for i in range(3):
-
-        source =  row[i]
-        token = source.split('/')
-        local_path = './my_data/IMG/'
-        file_path = token[-1]
-        local_path = local_path+file_path
-        img = cv2.imread(local_path)
-        imgs.append(img) 
-    steering = float(row['steering'])
-    labels.append(steering)
-    labels.append(steering + 0.2)
-    labels.append(steering - 0.2)
-augmented_images, augmented_measurements = [], []
-
-for index, row in my_reader_offroad.iterrows():
+for index, row in recovery_reader.iterrows():
     # print(row['center'], row['steering'])
     for i in range(3):
         source =  row[i]
         token = source.split('/')
-        local_path = './my_data_2/IMG/'
+        local_path = './track1/recovery/IMG/'
         file_path = token[-1]
         local_path = local_path+file_path
         img = cv2.imread(local_path)
         imgs.append(img)
-    steering = float(row['steering'])
-    labels.append(steering)
-    labels.append(steering + 0.2)
-    labels.append(steering - 0.2)
-  
-for index, row in my_reader_offroad_2.iterrows():
-    # print(row['center'], row['steering'])
-    for i in range(3):
-        source =  row[i]
-        token = source.split('/')
-        local_path = './my_data_3/IMG/'
-        file_path = token[-1]
-        local_path = local_path+file_path
-        img = cv2.imread(local_path)
     steering = float(row['steering'])
     labels.append(steering)
     labels.append(steering + 0.2)
@@ -103,20 +87,20 @@ def main(_):
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((70,20), (0, 0))))  # also supports shape inference using `-1` as dimension
     model.add(GaussianNoise(sigma=0.5))
-    model.add(Convolution2D(3, 5, 5, subsample=(2, 2),W_regularizer=l2(.01 )))
+    model.add(Convolution2D(3, 5, 5, subsample=(2, 2),W_regularizer=l2(.001 )))
     model.add(ELU())
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), W_regularizer= l2(.01)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), W_regularizer= l2(.001)))
     model.add(ELU())
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2),W_regularizer=l2(.01)))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2),W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Convolution2D(64, 3, 3, W_regularizer=l2(.01)))
+    model.add(Convolution2D(64, 3, 3, W_regularizer=l2(.001)))
     model.add(ELU())
     model.add(Flatten())
-    model.add(Dense(100,W_regularizer=l2(.01)))
+    model.add(Dense(100,W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Dense(10,W_regularizer=l2(.01)))
+    model.add(Dense(10,W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Dense(1,W_regularizer=l2(.01)))
+    model.add(Dense(1,W_regularizer=l2(.001)))
 
     model.compile(loss='mse', optimizer=Adam(lr=FLAGS.learning_rate))
     print("Model summary:\n", model.summary())
