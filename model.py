b03e349a6d04fa39a33dabb5b2a6b42b8efcb992
solix <soheil.jahanshahi@gmail.com>
2017-03-26 19:42:21 +0200

    std, nrormalize augmented data

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 
-reader = pd.read_csv('./data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
-recovery_reader = pd.read_csv('./my_data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+reader = pd.read_csv('./track1/drive/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+recovery_reader = pd.read_csv('./track1/recovery/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 recovery_reader2= pd.read_csv('./my_data_2/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 imgs = []
 labels = []
@@ -16,7 +16,7 @@ for  index, row in reader.iterrows():
     for i in range(3):
         source =  row[i]
         token = source.split('/')
-        local_path = './data/IMG/'
+        local_path = './track1/drive/IMG/'
         file_path = token[-1]
         local_path = local_path+file_path
         img = cv2.imread(local_path)
@@ -32,7 +32,7 @@ for index, row in recovery_reader.iterrows():
     for i in range(3):
         source =  row[i]
         token = source.split('/')
-        local_path = './my_data/IMG/'
+        local_path = './track1/recovery/IMG/'
         file_path = token[-1]
         local_path = local_path+file_path
         img = cv2.imread(local_path)
@@ -85,6 +85,8 @@ from keras.layers.noise import GaussianNoise
 from keras import backend as K
 from keras.layers.advanced_activations import ELU
 from keras.regularizers import l2, activity_l2
+import matplotlib.pyplot as plt
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -92,14 +94,24 @@ flags.DEFINE_integer('epochs', 11, "The number of epochs.")
 flags.DEFINE_integer('batch_size', 256, "The batch size.")
 flags.DEFINE_float('learning_rate', 0.0001, "The batch size.")
 
+def plothistory (history_object):
+
+### plot the training and validation loss for each epoch
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
 
 def main(_):
     # inspired from Nvidia
     print('Build model...')
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((90,20), (0, 0))))  # also supports shape inference using `-1` as dimension
-    model.add(GaussianNoise(sigma=0.5))
+    model.add(GaussianNoise(sigma=0.2))
     model.add(Convolution2D(3, 5, 5, subsample=(2, 2),W_regularizer=l2(.001 )))
     model.add(ELU())
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), W_regularizer= l2(.001)))
@@ -120,7 +132,8 @@ def main(_):
 
     # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 1)
     datagen.fit(X_train)
-    model.fit_generator(datagen.flow(X_train,y_train,batch_size=FLAGS.batch_size),samples_per_epoch=len(X_train),nb_epoch=FLAGS.epochs,validation_data=(X_valid,y_valid))
+    history = model.fit_generator(datagen.flow(X_train,y_train,batch_size=FLAGS.batch_size),samples_per_epoch=len(X_train),nb_epoch=FLAGS.epochs,validation_data=(X_valid,y_valid),verbose=1)
+    plothistory(history)
     model.save('model.h5')
     print("Model is saves as model.h5")
 
