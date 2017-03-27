12d69f96d15dd9f0a2f18d0cd94949ad03d2970b
solix <soheil.jahanshahi@gmail.com>
2017-03-27 21:20:29 +0200

    new recovery training data

--- model.py
+++ model.py
@@ -9,23 +9,23 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 
-# reader = pd.read_csv('./data/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
+reader = pd.read_csv('./recovery/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 reader2 = pd.read_csv('./drive/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 imgs = []
 labels = []
-# for  index, row in reader.iterrows():
-#     for i in range(3):
-#         source =  row[i]
-#         token = source.split('/')
-#         local_path = './data/IMG/'
-#         file_path = token[-1]
-#         local_path = local_path+file_path
-#         img = cv2.imread(local_path)
-#         imgs.append(img)
-#     steering = float(row['steering'])
-#     labels.append(steering)
-#     labels.append(steering + 0.2)
-#     labels.append(steering - 0.2)
+for  index, row in reader.iterrows():
+    for i in range(3):
+        source =  row[i]
+        token = source.split('/')
+        local_path = './recovery/IMG/'
+        file_path = token[-1]
+        local_path = local_path+file_path
+        img = cv2.imread(local_path)
+        imgs.append(img)
+    steering = float(row['steering'])
+    labels.append(steering)
+    labels.append(steering + 0.2)
+    labels.append(steering - 0.2)
 
 for  index, row in reader2.iterrows():
     for i in range(3):
@@ -132,7 +132,7 @@ def main(_):
     model.compile(loss='mse', optimizer=Adam(lr=FLAGS.learning_rate))
     print("Model summary:\n", model.summary())
 
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 1)
+    model.fit(augmented_imgs, augmented_steerings, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 1)
     # datagen.fit(X_train)
     # model.fit_generator(generator(),samples_per_epoch=len(X_train),nb_epoch=FLAGS.epochs,validation_data=(X_valid,y_valid),verbose=1)
     # plothistory(history)
