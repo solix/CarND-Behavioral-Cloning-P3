06e360ab430802ef2f7bde5172b37c744c94cc89
solix <soheil.jahanshahi@gmail.com>
2017-03-28 11:34:37 +0200

    added fit generator

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
 import random
 
-import cv2
 import numpy as np
 import pandas as pd
+import cv2
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
 
 reader = pd.read_csv('./track1/recovery/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
 reader2 = pd.read_csv('./track1/drive/driving_log.csv', usecols=['center', 'left', 'right', 'steering'])
@@ -21,8 +25,8 @@ for  index, row in reader.iterrows():
         imgs.append(img)
     steering = float(row['steering'])
     labels.append(steering)
-    labels.append(steering + 0.25)
-    labels.append(steering - 0.25)
+    labels.append(steering + 0.2)
+    labels.append(steering - 0.2)
 
 for  index, row in reader2.iterrows():
     for i in range(3):
@@ -35,10 +39,10 @@ for  index, row in reader2.iterrows():
         imgs.append(img)
     steering = float(row['steering'])
     labels.append(steering)
-    labels.append(steering + 0.25)
-    labels.append(steering - 0.25)
+    labels.append(steering + 0.2)
+    labels.append(steering - 0.2)
 
-for  index, row in reader.iterrows():
+for  index, row in reader3.iterrows():
     for i in range(3):
         source =  row[i]
         token = source.split('/')
@@ -49,8 +53,8 @@ for  index, row in reader.iterrows():
         imgs.append(img)
     steering = float(row['steering'])
     labels.append(steering)
-    labels.append(steering + 0.25)
-    labels.append(steering - 0.25)
+    labels.append(steering + 0.2)
+    labels.append(steering - 0.2)
 
 augmented_imgs = []
 augmented_steerings= []
@@ -63,11 +67,11 @@ for  img, msr in zip(imgs,labels):
     augmented_steerings.append(msr * -1.0)
 
 
-X_train = np.array(imgs)
-y_train = np.array(labels)
+X_train = np.array(augmented_imgs)
+y_train = np.array(augmented_steerings)
 
 
-# X_train,X_valid,y_train,y_valid = train_test_split(X_train,y_train,test_size=0.25)
+X_train,X_valid,y_train,y_valid = train_test_split(X_train,y_train,test_size=0.25)
 
 print(len(X_train), 'number of training data features')
 print(len(y_train), 'number of training labeles')
@@ -76,13 +80,15 @@ print(len(y_train), 'number of training labeles')
 
 # Model is inspired by nvidia cnn model with a different tweaks
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D, Reshape, Activation
 from keras.optimizers import Adam
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
 from keras.layers.noise import GaussianNoise
 # define flags for epoch and batchsize
+from keras import backend as K
 from keras.layers.advanced_activations import ELU
-from keras.regularizers import l2
+from keras.regularizers import l2, activity_l2
 import matplotlib.pyplot as plt
 
 
@@ -122,29 +128,29 @@ def main(_):
     print('Build model...')
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Cropping2D(cropping=((70,20), (0, 0))))  # also supports shape inference using `-1` as dimension
+    model.add(Cropping2D(cropping=((60,20), (0, 0))))  # also supports shape inference using `-1` as dimension
     model.add(GaussianNoise(sigma=0.5))
-    model.add(Convolution2D(3, 5, 5, subsample=(2, 2),W_regularizer=l2(.02 )))
+    model.add(Convolution2D(3, 5, 5, subsample=(2, 2),W_regularizer=l2(.001 )))
     model.add(ELU())
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), W_regularizer= l2(.02)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), W_regularizer= l2(.001)))
     model.add(ELU())
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2),W_regularizer=l2(.02)))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2),W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Convolution2D(64, 3, 3, W_regularizer=l2(.02)))
+    model.add(Convolution2D(64, 3, 3, W_regularizer=l2(.001)))
     model.add(ELU())
     model.add(Flatten())
-    model.add(Dense(100,W_regularizer=l2(.02)))
+    model.add(Dense(100,W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Dense(10,W_regularizer=l2(.02)))
+    model.add(Dense(10,W_regularizer=l2(.001)))
     model.add(ELU())
-    model.add(Dense(1,W_regularizer=l2(.02)))
+    model.add(Dense(1,W_regularizer=l2(.001)))
 
     model.compile(loss='mse', optimizer=Adam(lr=FLAGS.learning_rate))
     print("Model summary:\n", model.summary())
 
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 2)
+    # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size,verbose = 2)
     # datagen.fit(X_train)
-    # model.fit_generator(generator(),samples_per_epoch=len(X_train),nb_epoch=FLAGS.epochs,validation_data=(X_valid,y_valid),verbose=1)
+    model.fit_generator(generator(),samples_per_epoch=len(X_train),nb_epoch=FLAGS.epochs,validation_data=(X_valid,y_valid),verbose=1)
     # plothistory(history)
     model.save('model.h5')
     print("Model is saves as model.h5")
