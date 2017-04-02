30ebda1b999b07bf0871be494373eae4b7b20d36
solix <soheil.jahanshahi@gmail.com>
2017-04-02 14:35:35 +0200

    added generator for real time augmenting

--- model.py
+++ model.py
@@ -198,7 +198,7 @@ def main(_):
         # model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=i, batch_size=FLAGS.batch_size,verbose = 1)
         # fits the model on batches with real-time data augmentation:
         model.fit_generator(datagen.flow(X_train, y_train, batch_size=32),
-                            samples_per_epoch=len(X_train), nb_epoch=i)
+                            samples_per_epoch=len(X_train)//32, nb_epoch=i)
         model_no = 'model_M'+str(i)+'.h5'
         model.save(model_no)
         print("Model is saves as {}".format(model_no))
