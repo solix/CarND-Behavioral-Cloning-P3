c077e48b43138e006f2d8e1c8072cf86d3cee6ac
solix <soheil.jahanshahi@gmail.com>
2017-04-02 19:33:35 +0200

    massive change, added preprocess and augmentation via generator

--- model.py
+++ model.py
@@ -52,7 +52,7 @@ def load_and_augment_image(image):
         angle_offset = - 2
     steering_angle = image['steering'] + angle_offset
     print(image_file)
-    image = cv2.imread(image_file)
+    image = cv2.imread('./data/'+image_file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image, steering_angle = preprocess.random_transform(image, steering_angle)
     return image, steering_angle
